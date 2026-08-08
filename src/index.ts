5b823fafc8716aa9616a702083c2eae774061a95
Deepsn <75620590+Deepsn@users.noreply.github.com>
2026-08-08 13:19:15 -0300

    feat: add games, catalog and robux paths to bundle lookups

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,17 @@ import { getBundleScripts } from "./helpers/get-bundle-scripts";
 import { getBundlesSourceMaps } from "./helpers/get-bundle-source-map";
 import { writeBundlesToDist } from "./helpers/write-bundles-to-dist";
 
+const pathnames = ["games/606849621/Jailbreak", "catalog", "upgrades/robux"];
+
 console.log("Downloading bundles and source maps...");
-const bundleScripts = await getBundleScripts("charts");
+const bundleScripts = new Map<string, string>();
+for (const pathname of pathnames) {
+	console.log(`Downloading bundles for ${pathname}...`);
+	const scripts = await getBundleScripts(pathname);
+	for (const [url, text] of scripts) {
+		bundleScripts.set(url, text);
+	}
+}
 
 console.log(`Downloaded ${bundleScripts.size} bundles.`);
 const bundleMapping = await getBundlesSourceMaps(bundleScripts);
